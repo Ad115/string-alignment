7ba255051837a9db626a2f06d7d65e53ad778758
Ad115 <a.garcia230395@gmail.com>
2016-05-25 21:09:01 -0500

    Added simple test script

---
+++ global-align-test.sh
@@ -0,0 +1,13 @@
+# /usr/bin/env bash
+
+# Compile the program
+gcc global-align.c -o global-align
+
+# Hand it a simple input
+global-align vintners writers --scores=M20I-1D-1R-1 --type=max
+
+# Same input, different parameters
+global-align vintners writers --scores=M-10I10D10R5 --type=min
+
+# Give it a more complex input
+#global-align TTTTGAGGTGCAGATAGCTTGCTTTATTTTGTTGTTACTATCTCAAGGA GAGACAGAGTCTCACTCTGTTGCACAGGCTGGAGTGCAGTGGCACAATC --scores=M-10I10D10R5 --type=min
